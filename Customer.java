9888b4251ca8dd5f4779501bf83f29be2c1912cf
BillProkopiou <150947592+BillProkopiou@users.noreply.github.com>
2025-11-24 02:16:09 +0200

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,49 @@
+public class Customer {
+
+    private int AFM;
+    private String fullName;
+    private int number;
+    private String email;
+
+    public Customer(int AFM , String fullName , int number , String email){
+
+        this.AFM = AFM;
+        this.fullName = fullName;
+        this.number = number;
+        this.email = email;
+
+    }
+
+    public int getAFM() {
+        return AFM;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setAFM(int AFM) {
+        this.AFM = AFM;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
