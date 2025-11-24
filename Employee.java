f267bc5cb8c437bac1100966a0331891f09a19f2
BillProkopiou <150947592+BillProkopiou@users.noreply.github.com>
2025-11-24 02:48:45 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+
+    public Employee(String fullName , String username , String email , String password){
+
+        this.fullName = fullName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
