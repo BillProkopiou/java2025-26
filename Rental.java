bab24de48f5717d0e1ed25dd722ac7878190eef4
BillProkopiou <150947592+BillProkopiou@users.noreply.github.com>
2025-11-30 20:27:16 +0200

    Add files via upload

--- Rental.java
+++ Rental.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Rental {
 
     private int rentalId;
@@ -10,6 +12,10 @@ public class Rental {
     //Constructor
 
     public Rental(int rentalId , Car car , Customer customer , String begin , String expire , Employee employee){
+        if(!car.isAvailable()){
+            System.out.println("This car cant be rented!");
+            return;
+        }
         this.rentalId = rentalId;
         car.setAvailability(false);
         this.car = car;
@@ -52,6 +58,10 @@ public class Rental {
     }
 
     public void setCar(Car car) {
+        if(!car.isAvailable()){
+            System.out.println("This car cant be rented!");
+            return;
+        }
         this.car = car;
     }
 
@@ -72,7 +82,35 @@ public class Rental {
     }
 
     @Override
-    public int hashCode() {
-        return rentalId;
+    public String toString() {
+        return "ID: " + rentalId + " Car: " + car + " Customer: " + customer + " Rented: " + begin + " Return by: " + expire + " Rented with the help of: " + employee;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+
+        if(!(obj instanceof Rental)){
+            return false;
+        }
+
+        Rental r = (Rental) obj;
+
+        return r.rentalId == this.rentalId;
+    }
+
+    public static void safeAddRental(ArrayList<Rental> a , Rental b){
+        for(Rental c :a){
+            if(c.rentalId == b.rentalId){
+                System.out.println("This rental has already been done!");
+                return;
+            }
+        }
+
+        a.add(b);
+
+    }
+
 }
